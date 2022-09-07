647e885be828cb8fe86ba65a38dea95d5e6bc5b1
Imran-Siddiquii <siddiquiimran132@gmail.com>
2022-09-07 18:18:51 +0530

    fix:fix image tag warning

--- src/component/projects/Recommendation.jsx
+++ src/component/projects/Recommendation.jsx
@@ -4,39 +4,39 @@ import React, { useState } from 'react'
 const recommend={
     Bollywood:[{
         name:"Don2",
-        image:"https://flxt.tmsimg.com/assets/p8934938_p_v8_ai.jpg",
+        image:"https://flxt.tmsimg.com/assets/p8934938_p_v8_ai.jpg"
     
 
 },{
     image:"https://www.yashrajfilms.com/images/default-source/Movies/Sultan/sultan_767x430.jpg?sfvrsn=529dcccc_4",
-    name:"Sultan",
+    name:"Sultan"
 
 },
 {
     image:"https://upload.wikimedia.org/wikipedia/en/f/f1/Dhoom_3_Film_Poster.jpg",
-    name:"Dhoom 3",
+    name:"Dhoom 3"
 },
 {
     image:"https://resize.indiatvnews.com/en/resize/newbucket/1200_-/2020/12/pjimage-48-1606906136.jpg",
-    name:"MBBS",
+    name:"MBBS"
 }
 ],
     Hollywood:[{
     image:"https://s3.amazonaws.com/static.rogerebert.com/uploads/movie/movie_poster/fast-five-2011/large_jwNWXqMMbvM55czSepwog6mxqM9.jpg",
-        name:"Fast Five",
+        name:"Fast Five"
 
 },
 {
 image:"https://images.moviesanywhere.com/4677177f6f0595289bc3e767e7b47459/1d6c6c73-ab1e-4453-969c-6a4e965ebb37.jpg",
-    name:"Avanger Endgame",
+    name:"Avanger Endgame"
 },
 {
 image:"https://upload.wikimedia.org/wikipedia/en/f/ff/MI_%E2%80%93_Fallout.jpg",
-    name:"Mission Impossible 6",
+    name:"Mission Impossible 6"
 },
 {
 image:"https://upload.wikimedia.org/wikipedia/en/b/b9/Spectre_2015_poster.jpg",
-    name:"Specter OO7",
+    name:"Specter OO7"
 }]
 }
 const movies=Object.keys(recommend);  // get object key  such as hollywood and bollowood
@@ -56,7 +56,7 @@ const [moviesList,setMoviesList]=useState('Bollywood')
     }
   return (<>
     <div className='container-fluid d-flex my-4 justify-content-center w-100 '>
-
+    <br />
     {
         movies.map((list)=>(
             <button className='btn btn-primary mx-5' onClick={()=>bringMovie(list)} key={list}>{list}</button>
@@ -67,7 +67,7 @@ const [moviesList,setMoviesList]=useState('Bollywood')
         {recommend[moviesList].map((movieList,index)=>(<>
            <div key={index}>
            <div className="card mx-4 " style={{width: "18rem"}}>
-  <img src={movieList.image} className="card-img-top"  height="170px" width="fit-content" alt="Card image cap" />
+  <img  src={movieList.image} alt="Card" className="card-img-top"  height="170px" width="fit-content"  />
   <div className="card-body">
     <p className="card-text text-center">{movieList.name}</p>
     
